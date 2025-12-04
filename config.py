fba211b0d9e9f557ad3d44a7dcb03d05c3ce0a99
aminobre <allenbamino@gmail.com>
2025-12-04 02:24:47 -0500

    ?????

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-FUTUUR_BASE_URL = "https://api.futuur.com"  # placeholder for later
-FUTUUR_API_KEY = ""                         # set if/when you get a key
+import os
 
-BANKROLL_USD = 1000.0        # tweak later
-EDGE_THRESHOLD = 0.02        # 2 percentage points
-RISK_MODE = "half"           # 'full' or 'half'
+FUTUUR_BASE_URL = os.getenv("FUTUUR_BASE_URL", "https://api.futuur.com")
+FUTUUR_API_KEY = os.getenv("FUTUUR_API_KEY", "")
+
+BANKROLL_USD = 1000.0
+EDGE_THRESHOLD = 0.02
+RISK_MODE = "half"
